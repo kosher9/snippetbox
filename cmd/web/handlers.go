2cc63a76327c6e98aeec3dc9773353b725fa1c7c
kosher9 <lionel.kossi@gmail.com>
2020-05-12 21:18:17 +0000

    Initial commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/home.page.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", 500)
+	}
+	w.Write([]byte("Hello from snippetBox"))
+}
+
+func ShowSnippet(w http.ResponseWriter, r *http.Request)  {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet (ID %d)", id)
+}
+
+func NewSnippet(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Display the new snippet form"))
+}
